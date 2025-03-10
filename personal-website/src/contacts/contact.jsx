6e98b2c85b7299d60b425bb8f9fd3d94dfac8d61
Amal Saju <amalsaju1998@gmail.com>
2025-03-10 21:25:31 +1100

    Added emailjs email service without UI stuff

--- personal-website/src/contacts/contact.jsx
+++ personal-website/src/contacts/contact.jsx
@@ -1,9 +1,57 @@
-export default function Contact() {
-    const serviceId = import.meta.env.VITE_SERVICE_ID;
-    return (
-        <div>
-            <h1>Contact Me</h1>
-            {serviceId}
-        </div>
-    )
-}
+import { useState } from "react";
+import emailjs from "@emailjs/browser";
+
+export default function Contact(){
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [stateMessage, setStateMessage] = useState(null);
+  const sendEmail = (e) => {
+    e.persist();
+    e.preventDefault();
+    setIsSubmitting(true);
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_SERVICE_ID,
+        import.meta.env.VITE_TEMPLATE_ID,
+        e.target,
+        import.meta.env.VITE_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          setStateMessage("Message sent!");
+          setIsSubmitting(false);
+          setTimeout(() => {
+            setStateMessage(null);
+          }, 3000); // hide message after 5 seconds
+        },
+        (error) => {
+          setStateMessage("Something went wrong, please try again later");
+          setIsSubmitting(false);
+          setTimeout(() => {
+            setStateMessage(null);
+          }, 3000); // hide message after 5 seconds
+        }
+      );
+
+    // Clears the form after sending the email
+    e.target.reset();
+  };
+  return (
+    <div>
+      <h1>Contact Me</h1>
+      <form onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <br />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <br />
+        <label>Message</label>
+        <textarea name="message" />
+        <br />
+        <input type="submit" value="Send" disabled={isSubmitting} />
+        {stateMessage && <p>{stateMessage}</p>}
+      </form>
+    </div>
+  );
+};
+
