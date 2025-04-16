1f86bde5b8318ad5f494a758d928e45c0843f382
Amal Saju Kalarickal <amalsaju1998@gmail.com>
2025-04-17 06:28:17 +1000

    Feature: Added option to filter projects based on type; Updated some test values

--- personal-website/src/projects/projects.jsx
+++ personal-website/src/projects/projects.jsx
@@ -1,16 +1,62 @@
 import ProjectCard from "./project-card";
 import data from "../projects/projects.json";
+import { useEffect, useState } from "react";
 
+// for searching, index the tags and split up search term and search within the index
+// The index will map to the projects
 export default function Projects() {
+
+  const [filterOption, setFilterOption] = useState('all');
+
+  const handleButtonClick = (value) => {
+    setFilterOption(value);
+    console.log(value);
+  }
+
+
+  const filteredMap = data.filter(item => {
+    if (filterOption == "all") {
+      return item;
+    } else if (filterOption == "game") {
+
+      return item.type.toLowerCase() == "game".toLowerCase();
+    } else if (filterOption == "web") {
+
+      return item.type.toLowerCase() == "web".toLowerCase();
+    } else if (filterOption == "desktop") {
+
+      return item.type.toLowerCase() == "desktop".toLowerCase();
+    }
+
+
+  })
+
   return (
     <div className="flex flex-col h-screen bg-primary-light dark:bg-primary-dark">
-      <div className="ml-30 mt-30 mb-20">
-        <h1 className="font-bold leading-snug tracking-tight text-text-light dark:text-text-dark text-3xl lg:max-w-3xl lg:text-5xl">
+      <div className="ml-30 mt-20 ">
+        <h1 className="font-bold leading-snug tracking-tight text-text-light dark:text-text-dark text-3xl lg:max-w-3xl 3xl:text-5xl">
           Projects
         </h1>
       </div>
+      <div className="flex justify-center flex-col gap-2 mb-20">
+        <div className="flex justify-center">
+          Search bar here
+        </div>
+        <div className="flex justify-center">
+          Filter options here
+        </div>
+        <div className="flex justify-center">
+          Type:
+          <select onChange={e => setFilterOption(e.target.value)} name="type" id="type">
+            <option value="all">All</option>
+            <option value="game">Games</option>
+            <option value="web">Web</option>
+            <option value="desktop">Desktop</option>
+          </select>
+        </div>
+      </div>
       <div className="flex flex-col pl-25 pr-10 items-end gap-15 bg-primary-light dark:bg-primary-dark md:grid md:grid-cols-4 md:justify-items-center">
-        {data.map((item) => (
+        {filteredMap.map((item) => (
           <ProjectCard
             key={item.id}
             name={item.name}
